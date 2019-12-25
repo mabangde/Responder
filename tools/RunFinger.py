aa78023dc04aa92d401628adaaf208797099c792
mabangde <linuxseclab@gmail.com>
2019-12-25 21:46:49 +0800

    Fix RunFinger.py ms17010 (jumbo)

--- tools/RunFinger.py
+++ tools/RunFinger.py
@@ -20,7 +20,9 @@ import multiprocessing
 from socket import *
 from odict import OrderedDict
 import optparse
+import binascii
 from RunFingerPackets import *
+from sys import exit
 
 __version__ = "0.8"
 
@@ -151,33 +153,36 @@ def SmbFinger(Host):
        pass
 
 
-def check_ms17_010(host):
-    s = socket(AF_INET, SOCK_STREAM)
+def check_ms17_010(Host):
+    
+    negotiate_protocol_request = binascii.unhexlify(
+        "00000054ff534d42720000000018012800000000000000000000000000002f4b0000c55e003100024c414e4d414e312e3000024c4d312e325830303200024e54204c414e4d414e20312e3000024e54204c4d20302e313200")
+    session_setup_request = binascii.unhexlify(
+        "00000063ff534d42730000000018012000000000000000000000000000002f4b0000c55e0dff000000dfff02000100000000000000000000000000400000002600002e0057696e646f7773203230303020323139350057696e646f7773203230303020352e3000")
     try:
+        s = socket(AF_INET, SOCK_STREAM)
         s.settimeout(Timeout)
         s.connect(Host)
-        h = SMBHeader(cmd="\x72",flag1="\x18", flag2="\x53\xc8")
-        n = SMBNego(Data = SMBNegoData())
-        n.calculate()
-        packet0 = str(h)+str(n)
-        buffer0 = longueur(packet0)+packet0
-        s.send(buffer0)
-        data = s.recv(2048)
-        if data[8:10] == "\x75\x00":
-            head = SMBHeader(cmd="\x25",flag1="\x18", flag2="\x07\xc8",uid=data[32:34],tid=data[28:30],mid="\xc0\x00")
-            t = SMBTransRAPData()
-            t.calculate()
-            packet1 = str(head)+str(t)
-            buffer1 = longueur(packet1)+packet1  
-            s.send(buffer1)
-            data = s.recv(2048)
-            if data[9:13] == "\x05\x02\x00\xc0":
-                return True
-            else:
-                return False
+        s.send(negotiate_protocol_request)
+        s.recv(1024)
+        s.send(session_setup_request)
+        data = s.recv(1024)
+        user_id = data[32:34]
+        tree_connect_andx_request = "000000%xff534d42750000000018012000000000000000000000000000002f4b%sc55e04ff000000000001001a00005c5c%s5c49504324003f3f3f3f3f00" % ((58 + len(Host[0])), user_id.encode('hex'), Host[0].encode('hex'))
+        s.send(binascii.unhexlify(tree_connect_andx_request))
+        data = s.recv(1024)
+        allid = data[28:36]
+        payload = "0000004aff534d422500000000180128000000000000000000000000%s1000000000ffffffff0000000000000000000000004a0000004a0002002300000007005c504950455c00" % allid.encode('hex')
+        s.send(binascii.unhexlify(payload))
+        data = s.recv(1024)
+        s.close()
+        if "\x05\x02\x00\xc0" in data:
+            return True
         else:
             return False
-    except Exception as err:
+        s.close()
+    except Exception as e:
+        print(e)
         return False
 
 
